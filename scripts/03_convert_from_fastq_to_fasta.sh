c1dfb9219d62e386444186962e6064e0f36b1bd8
Jacqueline Imperial <jbimperial@dons.usfca.edu>
2019-11-20 18:01:48 +0000

    Add scripts.

--- scripts/03_convert_from_fastq_to_fasta.sh
+++ scripts/03_convert_from_fastq_to_fasta.sh
@@ -2,4 +2,9 @@
 # you need to modify this to save the converted fasta file to a file
 # instead of printing to the screen
 # you'll need to turn this into a for loop too
-bioawk -c fastx '{print ">"$name"\n"$seq}' /data/trimmed/filename.trim.fastq
+for file in /data/my-illumina-sequences/trimmed/JI*.fastq
+do
+	echo Convert fastq to fasta file with bioawk
+	bioawk -c fastx '{print ">"$name"\n"$seq}' /data/my-illumina-sequences/trimmed/JI-M3_S36_L001_R1_001.trim.fastq > /data/my-illumina-sequences/trimmed-fasta/JI-M3_S36_L001_R1_001.trim.fasta
+	echo Done
+done 
